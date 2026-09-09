48e147857f2981733c387eeda7968b922b5d3a6b
T.M.V.L. WEERASEKARA <itt2024113@tec.rjt.ac.lk>
2026-09-09 16:05:29 +0530

    Smooth Scrolling

--- js/script.js
+++ js/script.js
@@ -76,3 +76,37 @@ if (registerForm && password && confirmPassword) {
     });
 
 }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const smoothLinks = document.querySelectorAll('a[href^="#"]');
+
+    smoothLinks.forEach(function (link) {
+
+        link.addEventListener("click", function (event) {
+
+            const targetId = this.getAttribute("href");
+
+            if (targetId === "#") {
+                return;
+            }
+
+            const target = document.querySelector(targetId);
+
+            if (target) {
+
+                event.preventDefault();
+
+                target.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start"
+                });
+
+            }
+
+        });
+
+    });
+
+});
