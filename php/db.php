3e32dc5b3326c68b0ec782ba2d14e3a51cbd2385
T.M.V.L. WEERASEKARA <itt2024113@tec.rjt.ac.lk>
2026-09-10 22:38:08 +0530

    Add database connection

---
+++ php/db.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "al_techhub";
+
+$conn = new mysqli(
+    $host,
+    $username,
+    $password,
+    $database
+);
+
+if ($conn->connect_error) {
+
+    die("Database connection failed: " . $conn->connect_error);
+
+}
+
+?>
