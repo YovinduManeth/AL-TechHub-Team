4d0d2ba64872e3b140675819e40f7aacf4011e83
itt2024017-sketch <itt2024017@tec.rjt.ac.lk>
2026-09-16 17:10:54 +0530

    Finalize profile navigation and styling

--- profile.php
+++ profile.php
@@ -492,3 +492,15 @@ $subject_stmt->close();
     </div>
 
 </main>
+
+
+<!-- Bootstrap JavaScript -->
+
+<script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+></script>
+
+
+</body>
+
+</html>
